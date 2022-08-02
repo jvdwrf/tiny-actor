785e4d9751136210092a24632d6c16e8a7a80e87
jvdwrf <jaspervdwerf@gmail.com>
2022-08-02 13:21:26 +0200

    add downcast method to address

--- src/address.rs
+++ src/address.rs
@@ -3,6 +3,7 @@ use event_listener::EventListener;
 use futures::{Future, FutureExt};
 use std::{
     fmt::Debug,
+    mem::ManuallyDrop,
     pin::Pin,
     sync::Arc,
     task::{Context, Poll},
@@ -33,6 +34,15 @@ where
         &self.channel
     }
 
+    fn into_parts(self) -> (Arc<C>, Option<EventListener>) {
+        let no_drop = ManuallyDrop::new(self);
+        unsafe {
+            let channel = std::ptr::read(&no_drop.channel);
+            let exit_listener = std::ptr::read(&no_drop.exit_listener);
+            (channel, exit_listener)
+        }
+    }
+
     /// Get a new [Address] to the [Channel].
     pub fn get_address(&self) -> Address<C> {
         self.channel.add_address();
@@ -46,7 +56,33 @@ impl<M> Address<Channel<M>>
 where
     M: Send + 'static,
 {
+    /// Convert the `Address<Channel<M>>` into an `Address`.
+    pub fn into_dyn(self) -> Address {
+        let (channel, exit_listener) = self.into_parts();
+        Address {
+            channel,
+            exit_listener,
+        }
+    }
+
     gen::send_methods!();
+}
+
+impl Address {
+    /// Attempt to the downcast the `Address` into an `Address<Channel<M>>`.
+    pub fn downcast<M: Send + 'static>(self) -> Result<Address<Channel<M>>, Self> {
+        let (channel, exit_listener) = self.into_parts();
+        match channel.clone().into_any().downcast() {
+            Ok(channel) => Ok(Address {
+                channel,
+                exit_listener,
+            }),
+            Err(_) => Err(Self {
+                channel,
+                exit_listener,
+            }),
+        }
+    }
 }
 
 impl<C: DynChannel + ?Sized> Future for Address<C> {
